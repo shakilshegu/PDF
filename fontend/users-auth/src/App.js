8f8fad68e5454b4a849dc6a431851e59ae8c4c76
shakilshagu <muhammadshakil1968@gmail.com>
2023-11-01 11:33:02 +0530

    new commit

--- fontend/users-auth/src/App.js
+++ fontend/users-auth/src/App.js
@@ -1,12 +1,18 @@
 import { Routes,Route } from 'react-router-dom';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import Home from './pages/Home'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Routes>
-        
+        <Route path='/' element={<Home/>}> </Route>
+        <Route path='/login' element={<Login/>}> </Route>
+        <Route path='/signup' element={<SignUp/>}> </Route>
       </Routes>
     </div>
   );
