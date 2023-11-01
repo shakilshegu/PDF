f64226372ac682d66295f065cee2fe37046408f6
shakilshagu <muhammadshakil1968@gmail.com>
2023-11-01 12:16:01 +0530

    new commit

--- fontend/users-auth/src/components/Navbar.js
+++ fontend/users-auth/src/components/Navbar.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import {Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+      <nav className="flex justify-around p-3 border-b border-zinc-800 items-center bg-slate-800 text-zinc-200">
+        <Link to="/">
+          <h1 className="text-red-600 text-3xl">PDF</h1>
+        </Link>
+        <ul className="flex gap-6">
+          <Link to="/login">Login</Link>
+          <Link to="/signup">SignUp</Link>
+        </ul>
+      </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
