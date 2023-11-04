4c2eed0763008e8ebfa6cfdf7f6ddb0e0f756f8c
shakilshagu <muhammadshakil1968@gmail.com>
2023-11-04 20:38:56 +0530

    new commit

--- fontend/users-auth/src/pages/Login.js
+++ fontend/users-auth/src/pages/Login.js
@@ -3,7 +3,26 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      
+         <div className="w-full h-screen flex">
+      <div className="w-[50%] h-[100%] bg-black text-white flex justify-center items-center">
+        <form className="text-center border rounded-lg w-[600px] h-[400px] p-9">
+          <label>Username</label>
+          <br />
+          <input className="w-[400px] h-[40px] rounded-xl bg-zinc-700 p-2" />
+          <br />
+          <br />
+          <label>password</label>
+          <br />
+          <input className="w-[400px] h-[40px] rounded-xl bg-zinc-700 p-2" />
+          <br />
+          <br />
+          <button className="w-[200px] h-[50px] rounded-xl bg-zinc-300 p-2">Sign Up</button>
+        </form>
+      </div>
+      <div className="w-[50%] h-[100%] flex justify-center items-center bg-slate-600 ">
+       <h2 className="text-3xl text-white"> Login In </h2>
+      </div>
+    </div>  
     </div>
   )
 }
