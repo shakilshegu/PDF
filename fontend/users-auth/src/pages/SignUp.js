09c29d084f22bb609dc3f737234e611c69cdcd93
shakilshagu <muhammadshakil1968@gmail.com>
2023-11-04 20:35:33 +0530

    new commit

--- fontend/users-auth/src/pages/SignUp.js
+++ fontend/users-auth/src/pages/SignUp.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from "react";
 import axios from 'axios'
+import { Axios } from "../api/axioslnstance";
 
 const SignUp = () => {
  const [user,setUsers]= useState([])
