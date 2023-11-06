d833a6e96033522ab1ccdc96ef82de95e07762a6
shakilshagu <muhammadshakil1968@gmail.com>
2023-11-06 15:46:17 +0530

    new commit

---
+++ fontend/users-auth/utils/ProtectedRoutes.js
@@ -0,0 +1,11 @@
+import {Navigate} from "react-router-dom"
+
+function ProtectedRoute(prop) {
+    if (localStorage.getItem("token")) {
+      return prop.children;
+    } else {
+      return <Navigate to="/login" />;
+    }
+  }
+
+  export default ProtectedRoute
